6b53f93865603462ea7e78605b56b9426957d199
Jaswanth0608 <115534593+Jaswanth0608@users.noreply.github.com>
2023-09-02 15:04:10 +0530

    Updates app.py

--- app.py
+++ app.py
@@ -1,40 +1,3 @@
-# import numpy as np
-# from flask import Flask, request, jsonify, render_template #render_template help in getting back to home page.
-# import pickle
-
-# app = Flask(__name__)
-# model = pickle.load(open("Customer_churn_model.pkl","rb"))
-
-# @app.route("/") #need to be used create any number of uri's
-# def home():
-#      return render_template("index.html")
-
-# def pred(pred_list):
-#      preds = np.array(pred_list).reshape(1,6)
-#      loded_model = pickle.load(open("Customer_churn_model.pkl",'+rb'))
-#      result = loded_model.predict(preds)
-#      return result[0]
-
-# @app.route("/predict", methods=['POST'])
-# def predict():
-#      int_features = [float(x) for x in request.form.values()]
-#      # Age = request.form['Age']
-#      # Gender = request.form['Gender']
-#      # Location = request.form['Location']
-#      # Subscription_Length_Months = request.form['Subscription_Length_Months']
-#      # Monthly_Bill = request.form['Monthly_Bill']
-#      # Total_Usage_GB = request.form['Total_Usage_GB']
-#      features = [np.array(int_features)]
-#      prediction = model.predict(features)
-#      output = round(prediction[0],2)
-
-#      return render_template('home.html',prediction_text = 'The Customer will {}'.format(output))
-
-# if __name__ == "main":
-#      app.run()
-
-
-
 import pandas as pd
 import numpy as np
 from flask import Flask, request, render_template #render_template help in getting back to home page.
@@ -48,6 +11,15 @@ genders_ = ["Male", "Female"]
 locations_ = ['Los Angeles', 'New York', 'Miami', 'Chicago', 'Houston']
 
 app = Flask(__name__, template_folder='templates')
+
+def predictions(data_dict):
+     df = pd.DataFrame(data_dict)
+     df[['Location','Gender']] = df[['Location','Gender']].apply(lambda x: label_encoder.fit_transform(x))
+     df = minmaxscaler.transform(df)
+     print(df)
+     pred = model.predict(df)
+     print(pred,'\n',pred[0])
+     return pred[0]
 
 @app.route("/", methods=["GET"])
 def home():
@@ -64,13 +36,6 @@ def predict():
           Monthly_Bill = float(to_predict_list['Monthly_Bill'])
           Total_Usage_GB = float(to_predict_list['Total_Usage_GB'])
           print(Age,Gender,Location,Subscription_Length_Months,Monthly_Bill,Total_Usage_GB)
-          # result = _predict(Age,Gender,Location,Subscription_Length_Months,Monthly_Bill,Total_Usage_GB)
-          # print(Gender,Location)
-          # Gender_ = ""
-          # Location_ = ""
-          # Gender_ = genders_[Gender]
-          # Location_ = locations_[Location]   
-          # print(Gender_,Location_)
 
           data_dict = {
           'Age': [Age],
@@ -81,19 +46,7 @@ def predict():
           'Total_Usage_GB': [Total_Usage_GB]
           }
 
-          print(data_dict)
-
-          # Convert the dictionary to a DataFrame
-          df = pd.DataFrame(data_dict)
-          print (df)
-          df[['Location','Gender']] = df[['Location','Gender']].apply(lambda x: label_encoder.fit_transform(x))
-          print(df)
-          df = minmaxscaler.transform(df)
-          print(df)
-          pred = model.predict(df)
-          print(pred,'\n',pred[0])
-          result = pred[0]
-
+          result = predictions(data_dict)
      return render_template('index.html', 
                             age=Age,
                             gender=Gender,
